0e11d485c578b8139f7276a8698f79633ffb107b
herwin saputra <herwilkulit0@gmail.com>
2024-11-11 18:50:32 +0700

    learn git and github

---
+++ nur.php
@@ -0,0 +1,7 @@
+<h1>NAMA:nur haiki</h1>
+<h1>NPM:G1C024068 </h1>
+<h1>programstudi:teknik mesin</h1>
+<h1>hobi:volly</h1>
+<h1>matakulia:coding</h1>
+<h1>klas:B</h1>
+<h1>kata_katamutiara:hinaan mu semangat ku,mantap men </h1>
